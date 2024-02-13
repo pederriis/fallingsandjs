7fdeb173870f557cfd5375836329dfb30287be71
Peder Riis <pede0821@koldinghfogvuc.dk>
2024-02-13 13:17:33 +0100

    done maybe

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -9,10 +9,20 @@ function make2DArry(cols, rows){
 let grid;
 let w= 10;
 let cols, rows;
+let hueValue = 200;
 
+function withinCols(i){
+  return i>=0 && i <=cols-1; 
+}
+
+
+function withinRows(j){
+  return j>=0 && j <=rows-1; 
+}
 
 function setup() {
   createCanvas(400,400);
+  colorMode(HSB, 360, 255, 255);
  cols = width/w;
  rows=width/w;
  grid = make2DArry(cols,rows);
@@ -24,24 +34,44 @@ function setup() {
  }
 
 }
-function mouseClicked(){
-  let col = floor(mouseX/w);
-  let row = floor(mouseY/w);
+function mouseDragged(){
+  let mouseCol = floor(mouseX/w);
+  let mouseRow = floor(mouseY/w);
 
-  if (col>0 && col <=cols-1 &&row <rows-1  ){
-grid[col][row]=1;
+ 
+    let matrix = 3;
+    let extent = floor(matrix/2);
 
+    for (let i = -extent; i<=extent; i++){
+      for (let j = -extent; j<=extent; j++){
+        if (random(1)<0.75){
+          let col =mouseCol +i;
+          let row = mouseRow +j;
+          if(withinCols(col)&&withinRows(row)){
+            grid[col][row] = hueValue;
+          }
+        }
+      }
+      
+    }
+   
+    hueValue += 1;
+    if (hueValue > 360) {
+      hueValue = 1;
+    }
 }
-}
+
 function draw() {
   background(220);
   for (let i=0; i<cols; i++){
     for (let j=0; j<rows; j++){
-     stroke(255)
-     fill(grid[i][j]*255);
-     let x = i * w;
-     let y = j * w;
-    square(x,y,w);
+     noStroke()
+     if(grid[i][j]>0){
+       fill(grid[i][j]*300,100);
+       let x = i * w;
+       let y = j * w;
+      square(x,y,w);
+     }
 
     }
   }   
@@ -53,42 +83,58 @@ function draw() {
     }
    }
   
+
+
+
    for (let i=0; i<cols; i++){
     for (let j=0; j<rows; j++){
 
-      //du skal kun gøre noget hvis nuværende state er fyldt
-     if (grid[i][j]==1){
-
-      //hvis den nedenunder er fri, så fyld den ud
-       if(grid[i][j+1]===0 && j< rows-1){
-
-        
-        nextGrid[i][j+1]=1;  
+      let state = grid[i][j];
       
+      //her har den opdaget et fyldt felt der er på vej ned
+     if (state>0){
+      let below = grid[i][j+1];
+
+      let dir = 1;
+      if (random(1) < 0.5) {
+        dir *= -1;
       }
-       else{
-       let belowL=grid[i-1][j+1];
-       let belowR=grid[i+1][j+1];
 
-        if (belowL==0 && i-1>0){
-      
-          nextGrid[i-1][j+1]=1;
-        }
-        
-       
+      let belowA = -1;
+      let belowB = -1;
+          if (withinCols(i+dir)){
+          belowA=grid[i+dir][j+1];
+          }
           
-            nextGrid[i][j]=1
-          
-        }
+          if (withinCols(i-dir)){
+            belowB = grid[i-dir][j+1];
+          }
+
+          if (below===0){
+            nextGrid[i][j+1] = state;
+
+          }
+          else if(belowA===0){
+            nextGrid[i+dir][j+1] = state;
+          }
+          else if(belowB===0){
+            nextGrid[i-dir][j+1] = state;
+          }
+          else{
+            nextGrid[i][j]=state;
+          }
+      }
+
 
         
      }
     }
+    grid=nextGrid;
    }
-  
+   
 
-   grid=nextGrid;
-}
+   
+
 
  
 
